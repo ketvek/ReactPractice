634b2d51f397fdbdcf0736f42956a2d89035d270
Ketankumar Vekariya <140546769+ketvek@users.noreply.github.com>
2023-07-28 11:14:01 +0000

    Add Props in the component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./styles.css";
 import Hellofun from "./components/Hellofun";
 import HelloCla from "./components/HelloCla";
+import HelloProp from "./components/HelloProp";
 
 export default function App() {
   return (
     <div className="App">
       <Hellofun />
       <HelloCla />
+      <HelloProp name="Mango" />
     </div>
   );
 }
---
+++ src/components/HelloProp.js
@@ -0,0 +1,6 @@
+import react from "react";
+
+function HelloProp(props) {
+  return <h1> I love {props.name}</h1>;
+}
+export default HelloProp;
