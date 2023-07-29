c9347e41f63cb1c44572843520a13f3002acf672
Ketankumar Vekariya <140546769+ketvek@users.noreply.github.com>
2023-07-29 21:27:17 +0000

    Update App.js

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import "./styles.css";
 import Hellofun from "./components/Hellofun";
 import HelloCla from "./components/HelloCla";
 import HelloProp from "./components/HelloProp";
+import Comment from "./components/Comment";
 
 export default function App() {
   return (
@@ -9,6 +10,10 @@ export default function App() {
       <Hellofun />
       <HelloCla />
       <HelloProp name="Mango" weight="20gm" />
+      <Comment
+    date={comment.date}
+    text={comment.text}
+    author={comment.author} />
     </div>
   );
 }
