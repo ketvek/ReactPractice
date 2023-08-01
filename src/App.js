a2b67c1e349ffaa41c60dbce546710fdfc18b387
Ketankumar Vekariya <140546769+ketvek@users.noreply.github.com>
2023-08-01 09:54:17 +0000

    Examples from documentation

--- src/App.js
+++ src/App.js
@@ -1,28 +1,34 @@
 import "./styles.css";
-import Hellofun from "./components/Hellofun";
+import MyButton from "./components/MyButton";
+import Profile from "./components/Profile";
+/* import Hellofun from "./components/Hellofun";
 import HelloCla from "./components/HelloCla";
 import HelloProp from "./components/HelloProp";
 import Comment from "./components/Comment";
-
+ */
 export default function App() {
-  const comment = {
+  /* const comment = {
     date: new Date(),
     text: "I hope you enjoy learning React!",
     author: {
       name: "Hello Kitty",
       avatarUrl: "http://placekitten.com/g/64/64",
     },
-  };
+  }; */
+
   return (
     <div className="App">
-      <Hellofun />
+      {/*  <Hellofun />
       <HelloCla />
       <HelloProp name="Mango" weight="20gm" />
       <Comment
         date={comment.date}
         text={comment.text}
         author={comment.author}
-      />
+      /> */}
+      <h1>Welcome Guys</h1>
+      <Profile />
+      <MyButton />
     </div>
   );
 }
---
+++ src/components/MyButton.js
@@ -0,0 +1,6 @@
+import react from "react";
+
+function MyButton() {
+  return <button>I am The Button</button>;
+}
+export default MyButton;
